4593e3109b58ede3f9ed766ef290b55961488290
Daniel van der Merwe <danieljmerwe@gmail.com>
2017-06-17 15:07:31 +0200

    Linting

--- src/api/index.js
+++ src/api/index.js
@@ -3,15 +3,15 @@ import { Router } from 'express';
 import facets from './facets';
 
 export default ({ config, db }) => {
-	let api = Router();
+    const api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+    // mount the facets resource
+    api.use('/facets', facets({ config, db }));
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+    // perhaps expose some API metadata at the root
+    api.get('/', (req, res) => {
+        res.json({ version });
+    });
 
-	return api;
-}
+    return api;
+};
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import middleware from './middleware';
 import api from './api';
 import config from './config.json';
 
-let app = express();
+const app = express();
 app.server = http.createServer(app);
 
 // logger
@@ -17,25 +17,24 @@ app.use(morgan('dev'));
 
 // 3rd party middleware
 app.use(cors({
-	exposedHeaders: config.corsHeaders
+    exposedHeaders: config.corsHeaders,
 }));
 
 app.use(bodyParser.json({
-	limit : config.bodyLimit
+    limit: config.bodyLimit,
 }));
 
 // connect to db
-initializeDb( db => {
+initializeDb((db) => {
+    // internal middleware
+    app.use(middleware({ config, db }));
 
-	// internal middleware
-	app.use(middleware({ config, db }));
+    // api router
+    app.use('/api', api({ config, db }));
 
-	// api router
-	app.use('/api', api({ config, db }));
-
-	app.server.listen(process.env.PORT || config.port, () => {
-		console.log(`Started on port ${app.server.address().port}`);
-	});
+    app.server.listen(process.env.PORT || config.port, () => {
+        console.log(`Started on port ${app.server.address().port}`);
+    });
 });
 
 export default app;
